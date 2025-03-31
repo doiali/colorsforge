b1128d70e50f4c30ab4eae3443c874061245aa8d
pooria <abagheri1994@gmail.com>
2025-03-31 16:38:43 +0200

    feat: update range input backgrounds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,21 +22,24 @@ export default function Home() {
     colord(state.rgb)
 
   return (
-    <main className="flex flex-col items-center py-12">
-      <h1 className="text-4xl font-bold">Color Picker</h1>
+    <main className="flex flex-col py-12 container max-w-2xl mx-auto px-4">
+      <h1 className="text-4xl font-bold mb-4">Color Picker</h1>
       <Tabs defaultValue='hsl'>
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="rgb">RGB</TabsTrigger>
           <TabsTrigger value="hsl">HSL</TabsTrigger>
         </TabsList>
-        <TabsContent value="rgb">
-          <ColorPickerRGB value={state} onChange={setState} />
-        </TabsContent>
-        <TabsContent value="hsl">
-          <ColorPickerHSL value={state} onChange={setState} />
-        </TabsContent>
+        <div className="pt-6">
+          <TabsContent value="rgb">
+            <ColorPickerRGB value={state} onChange={setState} />
+          </TabsContent>
+          <TabsContent value="hsl">
+            <ColorPickerHSL value={state} onChange={setState} />
+          </TabsContent>
+        </div>
       </Tabs>
       <div>
+        <div className="h-32" style={{ backgroundColor: color.toHex() }}></div>
         <div className="mt-4 space-y-2 text-center">
           <p><strong>RGB:</strong> {color.toRgbString()}</p>
           <p><strong>HSL:</strong> hsl({h}, {s}%, {l}%)</p>
@@ -60,10 +63,49 @@ const ColorPickerRGB: React.FC<{
     }))
   }
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <input name="r" type="range" min="0" max="255" value={r} onChange={handleChange} />
-      <input name="g" type="range" min="0" max="255" value={g} onChange={handleChange} />
-      <input name="b" type="range" min="0" max="255" value={b} onChange={handleChange} />
+    <div className="flex flex-col">
+      <label htmlFor="rgb-r-range" className="flex justify-between">R (Red) <span className="inline-flex h-6 w-6" style={{ backgroundColor: `rgb(${r},0,0)` }} /></label>
+      <input
+        id="rgb-r-range"
+        className="mb-4 appearance-none mt-1 w-full h-2 rounded-lg"
+        name="r"
+        type="range"
+        min="0"
+        max="255"
+        value={r}
+        onChange={handleChange}
+        style={{
+          background: 'linear-gradient(to right, black, red)',
+        }}
+      />
+      <label htmlFor="rgb-g-range" className="flex justify-between">G (Green) <span className="inline-flex h-6 w-6" style={{ backgroundColor: `rgb(0,${g},0)` }} /></label>
+      <input
+        id="rgb-g-range"
+        className="mb-4 appearance-none mt-1 w-full h-2 rounded-lg"
+        name="g"
+        type="range"
+        min="0"
+        max="255"
+        value={g}
+        onChange={handleChange}
+        style={{
+          background: 'linear-gradient(to right, black, green)',
+        }}
+      />
+      <label htmlFor="rgb-b-range" className="flex justify-between">B (Blue) <span className="inline-flex h-6 w-6" style={{ backgroundColor: `rgb(0,0,${b})` }} /></label>
+      <input
+        id="rgb-b-range"
+        className="mb-4 appearance-none mt-1 w-full h-2 rounded-lg"
+        name="b"
+        type="range"
+        min="0"
+        max="255"
+        value={b}
+        onChange={handleChange}
+        style={{
+          background: 'linear-gradient(to right, black, blue)',
+        }}
+      />
     </div>
   )
 }
@@ -82,10 +124,49 @@ const ColorPickerHSL: React.FC<{
   }
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <input name='h' type="range" min="0" max="360" value={h} onChange={handleChange} />
-      <input name='s' type="range" min="0" max="100" value={s} onChange={handleChange} />
-      <input name='l' type="range" min="0" max="100" value={l} onChange={handleChange} />
+    <div className="flex flex-col">
+      <label htmlFor="hsl-h-range" className="flex justify-between">H (Hue) <span className="inline-flex h-6 w-6" style={{ backgroundColor: `hsl(${h},100%,50%)` }} /></label>
+      <input
+        id="hsl-h-range"
+        className="mb-4 appearance-none mt-1 w-full h-2 rounded-lg"
+        name="h"
+        type="range"
+        min="0"
+        max="360"
+        value={h}
+        onChange={handleChange}
+        style={{
+          background: 'linear-gradient(to right, red, yellow, lime, cyan, blue, magenta, red)',
+        }}
+      />
+      <label htmlFor="hsl-s-range" className="flex justify-between">S (Saturation) <span className="inline-flex h-6 w-6" style={{ backgroundColor: `hsl(${h},${s}%,50%)` }} /></label>
+      <input
+        id="hsl-s-range"
+        className="mb-4 appearance-none mt-1 w-full h-2 rounded-lg"
+        name="s"
+        type="range"
+        min="0"
+        max="100"
+        value={s}
+        onChange={handleChange}
+        style={{
+          background: `linear-gradient(to right, hsl(${h},0%,50%), hsl(${h},100%,50%))`,
+        }}
+      />
+      <label htmlFor="hsl-l-range" className="flex justify-between">L (Lightness) <span className="inline-flex h-6 w-6" style={{ backgroundColor: `hsl(${h},100%,${l}%)` }} /></label>
+      <input
+        id="hsl-l-range"
+        className="mb-4 appearance-none mt-1 w-full h-2 rounded-lg"
+        name="l"
+        type="range"
+        min="0"
+        max="100"
+        value={l}
+        onChange={handleChange}
+        style={{
+          background: `linear-gradient(to right, black, hsl(${h},100%,50%), white)`,
+        }}
+      />
     </div>
   )
 }
